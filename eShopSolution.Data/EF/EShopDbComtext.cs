61e3065831a7f0dc8c0f7ab5a2faab7d299f84fa
truong19961405 <54049414+truong19961405@users.noreply.github.com>
2021-12-24 23:27:05 +0700

    Configuration

--- eShopSolution.Data/EF/EShopDbComtext.cs
+++ eShopSolution.Data/EF/EShopDbComtext.cs
@@ -1,4 +1,5 @@
-﻿using eShopSolution.Data.Entities;
+﻿using eShopSolution.Data.Configurations;
+using eShopSolution.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,15 @@ namespace eShopSolution.Data.EF
         public EShopDbComtext(DbContextOptions options ): base(options)
         {
 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+
+            //base.OnModelCreating(modelBuilder);
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
--- eShopSolution.Data/Entities/OrderDetail.cs
+++ eShopSolution.Data/Entities/OrderDetail.cs
@@ -16,5 +16,7 @@ namespace eShopSolution.Data.Entities
         public Order Order { get; set; }
 
         public Product Product { get; set; }
+        
+
     }
 }
